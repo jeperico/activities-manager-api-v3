5ec5f693f966c1bedb3d0a5347fa41c6fa02cc4a
João Périco <joaoeperico@gmail.com>
2025-01-29 16:44:50 -0300

    [style] remove spaces

--- app/urls.py
+++ app/urls.py
@@ -5,15 +5,15 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path(
-      'schema/',
-      SpectacularAPIView.as_view(),
-      name='schema',
-    ),
-    path(
-      'schema/swagger/',
-      SpectacularSwaggerView.as_view(url_name='schema'),
-      name='swagger-ui',
-    ),
+  path(
+    'schema/',
+    SpectacularAPIView.as_view(),
+    name='schema',
+  ),
+  path(
+    'schema/swagger/',
+    SpectacularSwaggerView.as_view(url_name='schema'),
+    name='swagger-ui',
+  ),
 ]
 
